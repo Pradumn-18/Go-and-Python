7a7bf52f5c3fe771a9bd75601c881b5d3973747c
Pradumn-18 <44301497+Pradumn-18@users.noreply.github.com>
2020-06-08 20:25:53 +0530

    Create test_serve.py

---
+++ script/test_serve.py
@@ -0,0 +1,21 @@
+import os,sys
+from datetime import datetime
+import copy
+import json
+import requests
+import time
+import pytest
+import sys
+
+e_url="http://localhost:8090"
+
+#test Case 1
+'''
+  Testing the /hello route
+'''
+def test_route1():
+        url=e_url+"/hello"
+        r=requests.post(url)
+        print(r.status_code)
+        print(r.content)
+        assert r.status_code == 200
