a0998f44972a9075ba8ddf2e06fd401982bda18e
Pradumn-18 <44301497+Pradumn-18@users.noreply.github.com>
2020-06-11 00:33:35 +0530

    Update try.sh

--- try.sh
+++ try.sh
@@ -23,9 +23,5 @@ sudo docker ps -a
 cd v2
 pytest -s -v
 
-sudo docker stop -d nginx:latest
-sudo docker stop -d rabbitmq:3
-sudo docker stop -d fogflow/discovery:3.0
-sudo docker stop -d fogflow/broker:3.0
 
 
